63d78381c2e71794e23965810c55df666cf9658e
szouc <qdshizh@gmail.com>
2018-12-17 08:31:17 +0800

    change orders and vehicles quantities

--- tmp.py
+++ tmp.py
@@ -2,13 +2,13 @@ from learning.q_learning import q_learning
 from envs import LogisticsEnv
 from utils import plotting
 
-orders = 10
+orders = 100
 vehicles = 10
 kinds = 3
 
 env = LogisticsEnv(orders=orders, vehicles=vehicles, kinds=kinds)
 
-Q, stats = q_learning(env=env, num_episodes=1000,
+Q, stats = q_learning(env=env, num_episodes=100 ,
                       discount_factor=0.9, alpha=0.7, epsilon=0.1)
 
 plotting.plot_episode_stats(stats)
